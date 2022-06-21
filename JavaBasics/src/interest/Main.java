b3c946b31dc165e5139fa606624a1f8435fcf5b1
unknown <hana@N-AV168.hanabank.co.id>
2022-06-21 20:14:39 +0700

    [Homework 21 June 2022] Update the new homework for candy class & simple interest calculator

--- JavaBasics/src/interest/Main.java
+++ JavaBasics/src/interest/Main.java
@@ -2,8 +2,10 @@ package interest;
 
 public class Main {
     public static void main(String[] args) {
+//        Define the new object
         Interest x = new Interest();
 
+//        Call the method from class
 //        x.countInterest();
         int calculateInterest = x.countInterest(10,10,10);
         System.out.println("Result for the Interest is : ");
